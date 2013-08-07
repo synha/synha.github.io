8eafa7b2539f7f93aea5aaa4ca9d94e3d4c57011
Viraj Sinha <synhapse@gmail.com>
2013-08-07 09:06:23 -0400

    everything works!

---
+++ _posts/2013-08-06-simulated-annealer.md
@@ -0,0 +1,76 @@
+---
+layout: post
+title: "Simulated Annealer"
+description: ""
+category: 
+tags: []
+---
+
+This solves the Traveling Salesman problem for the inputted number of cities. The solution will tend towards the best one as runtime goes to infinity. In practice, this annealer gets good solutions in well under infinite time (although it may take a few runs to converge).
+
+<!--more-->
+
+Instructions: Click "Start!" first.
+
+<div>
+	<canvas id="canvas1" width="640" height="480" style="border:1px solid #c3c3c3;">
+		Your browser doesn't appear to support the HTML5 <code>&lt;canvas&gt;</code> element.
+	</canvas>
+	<br>
+	
+
+	<table border='0'>
+
+		<tr>
+			<td>Number of Cities:</td>
+			<td><input id ="citiesBox" type="text"/></td>
+			<td>Start Temperature:</td>
+			<td><input id ="tempBox" type="text"/></td>
+		</tr>
+		<tr>
+			<td><button onclick="start()">Start!</button></td>
+			<td><button onclick="repick()">Select new cities</button></td>
+			<td><button onclick="resimulate()">Rerun simulation <br>(same cities)</button></td>
+			<td><button onclick="displayFinalSolution()">Jump to best solution <br> found so far</button></td>
+		</tr>
+		<!-- for alternate cooling schedules
+		<tr>
+			<td>cooling schedule:</td>
+			<td><select id="dropDown" onchange="">
+				<option>Linear</option>
+				<option>Quadratic</option>
+				<option>Logarithmic</option>
+				
+			</select></td>
+		</tr>
+		-->
+		<tr>
+			<td>Temp:</td>
+			<td id="p1"></td>
+		</tr>
+		<tr>
+			<td>Current Energy:</td>
+			<td id="p2"></td>
+		</tr>
+		<tr>
+			<td>Best Energy:</td>
+			<td id="p3"></td>
+			<!--
+			<td>progress:</td>
+			<td id="p4"></td>
+			-->
+		</tr>
+	</table>
+	
+	
+	<script src="/js/simulated_anneal.js" type="text/javascript">start();</script>
+
+</div>
+
+The idea of temperature here draws from the analogy that we're cooling a dynamic particle system (liquid metal for instance) and as the system crystallizes, the particles fall into their optimal - ie, lowest energy - states.
+
+Keeping track of the overall lowest energy state for a given system of cities is a metaheuristic technically outside a 'pure' simulated annealer, since in a particle system a given particle has no way to save its state. It's easy in software though, so the rightmost button will move the system to reflect the lowest energy state found so far.
+
+I just finished taking a CS class called Numerical Methods. We covered various (numerical) methods to solve equations, systems of linear/nonlinear/differential equations, optimization, curve fitting, splines, and simulated annealing.
+
+I'd like to add support for different cooling schedules, the one used in the simulation right now is a simple linear decrease in temperature.
---
+++ js/simulated_anneal.js
@@ -0,0 +1,264 @@
+var c = document.getElementById("canvas1");
+var p1 = document.getElementById("p1");
+var p2 = document.getElementById("p2");
+var p3 = document.getElementById("p3");
+//var p4 = document.getElementById("p4");
+var dropDown = document.getElementById("dropDown");
+var tempBox = document.getElementById("tempBox");
+var citiesBox = document.getElementById("citiesBox");
+var width = c.width;
+var height = c.height;
+var ctx = c.getContext("2d");
+var cities = [];
+var numPts = width/2;
+var numCities = 10;
+var bestSol;
+var best = Number.MAX_VALUE; //start big
+var startTemp = 100;
+var temp = startTemp;
+
+function coordinate(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+//so we can get random elements easily
+Array.prototype.randomElement = function () {
+	return this[Math.floor(Math.random() * this.length)];
+};
+
+//to copy the array
+Array.prototype.clone = function() {
+	return this.slice(0);
+};
+
+//let's do this with the travelling salesman problem
+function generateCities() {
+	var i, newX, newY;
+	for (i = 0; i < numCities; i++) {
+		newX = Math.random() * (width - 10) + 10;
+		newY = Math.random() * (height - 10) + 10;
+		cities.push(new coordinate(newX, newY));
+	}
+}
+
+function drawCircle(x, y) {
+	ctx.beginPath();
+    ctx.arc(x, y, 3, 0, 2 * Math.PI, false);
+    ctx.fillStyle = '#0000ff';
+    ctx.fill();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#444444';
+    ctx.stroke();
+}
+
+function drawCities() {
+	var i;
+	for (i = 0; i < numCities; i++) {
+		drawCircle(cities[i].x, cities[i].y);
+	}
+}
+
+function drawLine(city1, city2) {
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = '#aaaaaa';
+	var i;
+
+	for (i = 0; i < numCities - 1; i++) {
+		ctx.beginPath();
+		ctx.moveTo(city1.x, city1.y); 
+		ctx.lineTo(city2.x, city2.y);
+		ctx.stroke();
+	}
+}
+
+
+//draw the connecting line between cities
+function connectCities() {
+	var i;
+	for (i = 0; i < numCities - 1; i++) {
+		drawLine(cities[i], cities[i+1]);
+	}
+}
+
+
+function setupGraph() {
+	var i;
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = '#DDDDDD';
+
+	for (i = 0; i < height; i+=50) { //draw horizontal line every 10 pixels for reference
+		ctx.beginPath();
+		ctx.moveTo(0, i);
+		ctx.lineTo(width, i);
+		ctx.stroke();
+	}
+
+	for (i = 0; i < width; i+=50) { //draw horizontal line every 10 pixels for reference
+		ctx.beginPath();
+		ctx.moveTo(i, 0);
+		ctx.lineTo(i, height);
+		ctx.stroke();
+	}
+}
+
+function energy(solution) { //sum distance between cities in order
+	var i;
+	var sum = 0;
+	for (i = 0; i < numCities-1; i++) {
+		//distance formula sqrt((x1 - x2)^2 + (y1 - y2)^2)
+		sum += Math.sqrt( Math.pow(solution[i].x - solution[i + 1].x, 2) + Math.pow(solution[i].y - cities[i+1].y, 2));
+	}
+	return sum;
+}
+
+//make a single change to the current cities list
+//randomly swap order of 2 cities
+//be stateful about it
+function neighbor() {
+	//pick 2 randoms
+	var rand1 = Math.floor(Math.random() * numCities);
+	var rand2 = rand1;
+	while (rand2 === rand1) {
+		rand2 = Math.floor(Math.random() * numCities);
+	}
+
+	//work with a clone of the cities array
+	var neighborSolution = cities.slice(0);
+
+	//swap them
+	var temp = neighborSolution[rand1];
+	neighborSolution[rand1] = neighborSolution[rand2];
+	neighborSolution[rand2] = temp;
+	return neighborSolution;
+}
+
+function probFunc(neighborSolution, currentSolution, temp) {
+	return (Math.random() < Math.exp(-(energy(neighborSolution)-energy(currentSolution))/temp));
+}
+
+function redraw() {
+	//draw the indicator line if we have a better one
+	ctx.canvas.width = ctx.canvas.width; //reset graph?
+	setupGraph();
+	connectCities();
+	drawCities();
+	p2.innerHTML = energy(cities).toFixed(2);
+	//write to best solution div
+	if (energy(cities) < best) {
+		best = energy(cities);
+		bestSol = cities.slice(0);
+		p3.innerHTML = best.toFixed(2);
+	}
+}
+
+function simulateStep(temp) { //do it one step at a time for animation
+	var currentSolution = cities;
+	var neighborSolution = neighbor();
+	
+	if (energy(neighborSolution) < energy(currentSolution)) {
+		currentSolution = neighborSolution;
+	} else if (probFunc(neighborSolution, currentSolution, temp)) { //accept anyways
+		currentSolution = neighborSolution;
+	}
+
+	cities = currentSolution;  //store to global var
+
+	redraw();
+}
+
+function animate(opts) {
+	var start = new Date();   
+
+	var id = setInterval(function() {
+		var timePassed = new Date() - start;
+		var progress = timePassed / opts.duration;
+		//p4.innerHTML = progress.toFixed(2);
+
+		temp = opts.temp(temp, progress);
+
+		if (temp <= 0) {
+			temp = 0;
+		}
+
+		opts.step(temp);
+		p1.innerHTML = temp.toFixed(2);
+
+		if (temp === 0) {
+			clearInterval(id);
+			//displayFinalSolution();
+		}
+	}, opts.delay || 10);
+
+}
+
+//assorted cooling functions
+function linear(temp) {
+    return temp - 0.1;
+}
+
+function quadratic(temp, progress) {
+	return temp - 0.1*Math.pow(progress, 2);
+}
+
+function logarithmic(temp, progress) {
+	return temp + 0.1*Math.log(progress);
+}
+
+function selectCoolingFunc(temp, progress) {
+	switch (dropDown.options[dropDown.selectedIndex].text) {
+		case "Linear":
+			return linear(temp);
+		case "Quadratic":
+			return quadratic(temp, progress);
+		case "Logarithmic":
+			return logarithmic(temp, progress);
+	}
+}
+
+function simulateAnneal() {
+
+	animate({
+		delay: 1,
+    	duration: 1000, // 1 sec by default
+    	temp: function(temp, progress) {
+			return linear(temp);
+		},
+    	step: function(temp) {
+    		simulateStep(temp);
+    	}
+	});
+}
+
+function displayFinalSolution() {
+	cities = bestSol;
+	redraw();
+}
+
+function resimulate() {
+	startTemp = tempBox.value;
+	temp = startTemp;
+	simulateAnneal();
+}
+
+function repick() {
+	startTemp = tempBox.value;
+	numCities = citiesBox.value;
+	temp = startTemp;
+	cities = [];
+	bestSol = [];
+	best = Number.MAX_VALUE;
+	generateCities();
+	redraw();
+	resimulate();
+}
+
+function start() {
+	tempBox.value = startTemp;
+	temp = startTemp;
+	citiesBox.value = numCities;
+	generateCities();
+	redraw();
+	
+	simulateAnneal();
+}
